323911c240d2f253cb3d7f022e4ef2c798330b5b
Lucas Rocha <lucasr-conceicao@outlook.com.br>
2023-01-15 22:59:20 -0300

    Estrutura inicial
    Estrutura inicial do sqs microservice

---
+++ src/main/java/br/com/sqs/microservice/SqsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.sqs.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sqs/microservice/SqsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sqs.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
